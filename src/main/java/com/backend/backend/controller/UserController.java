51c72fba0777205434350ed781a806073a19fda4
einartech <imalovelyghost13@gmail.com>
2025-04-25 14:53:02 +0200

    feat(controller):Controller of entity User done

---
+++ src/main/java/com/backend/backend/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.backend.backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.backend.service.UserService;
+import com.backend.backend.model.User;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<User> addUser(@Valid @RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return new ResponseEntity<>(createdUser, HttpStatus.CREATED);
+    }
+
+}
