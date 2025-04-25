2a948624d44dfff57c00bde0df91101e6a144897
einartech <imalovelyghost13@gmail.com>
2025-04-25 14:29:55 +0200

    feat(repository):Repository of entity user base done

---
+++ src/main/java/com/backend/backend/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.backend.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.backend.backend.model.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
