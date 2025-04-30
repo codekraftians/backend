00435c10d28ae513f5990283cf732c3e0869549b
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-04-30 15:14:48 +0200

    feat(dotenv): implement dotenv and connection to DB, hotfix add BEAN service at UsereService

--- src/main/java/com/backend/backend/BackendApplication.java
+++ src/main/java/com/backend/backend/BackendApplication.java
@@ -2,11 +2,18 @@ package com.backend.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.*;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
 		SpringApplication.run(BackendApplication.class, args);
 		System.out.println("Hello World");
 	}
--- src/main/java/com/backend/backend/service/UserService.java
+++ src/main/java/com/backend/backend/service/UserService.java
@@ -1,8 +1,12 @@
 package com.backend.backend.service;
 
 import com.backend.backend.repository.UserRepository;
+
+import org.springframework.stereotype.Service;
+
 import com.backend.backend.model.User;
 
+@Service
 public class UserService {
     private final UserRepository userRepository;
 
