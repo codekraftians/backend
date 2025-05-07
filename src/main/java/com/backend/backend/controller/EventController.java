afaa240a38b203ddf7ac5b8327d518b7a7cc428e
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-07 14:49:30 +0200

    hotfix(dev): add bean crossorigin in EventController

--- src/main/java/com/backend/backend/controller/EventController.java
+++ src/main/java/com/backend/backend/controller/EventController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
 @RequestMapping("/api/v1/events")
+@CrossOrigin(origins = "http://localhost:5173")
 public class EventController {
 
     private final EventService eventService;
